aa43858467839f41fed2b25236ef573fe6aadaf5
Gleb Tregubov <tregubovgleb@gmail.com>
2023-03-05 21:03:24 +0300

    Initial Commit

---
+++ task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task2
+//
+//  Created by Gleb Tregubov on 05.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
